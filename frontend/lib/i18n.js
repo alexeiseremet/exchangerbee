68b48b7d49e4aec6084c2f3747274082560a083d
Alexei Seremet <alexei.seremet@gmail.com>
2019-04-25 14:09:24 +0300

    Update Modal styles & yarn packages.

--- frontend/lib/i18n.js
+++ frontend/lib/i18n.js
@@ -1,7 +1,8 @@
 const NextI18Next = require('next-i18next/dist/commonjs')
 
 module.exports = new NextI18Next({
-  otherLanguages: ['ro', 'ru'],
+  defaultLanguage: 'ro',
+  otherLanguages: ['en', 'ru'],
   localeSubpaths: 'all',
   ignoreRoutes: ['/_next', '/static', '/graphql', '/admin', '/storage'],
   serverLanguageDetection: false,
