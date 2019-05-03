fb69e8c300951031d5cd8e8d8539207448042764
Alexei Seremet <alexei.seremet@gmail.com>
2019-05-03 17:20:32 +0300

    Add Crawler v1.

---
+++ backend/crawler/index.js
@@ -0,0 +1,61 @@
+const puppeteer = require('puppeteer')
+const devices = require('puppeteer/DeviceDescriptors')
+const iPad = devices['iPad Pro landscape']
+
+const runCrawler = async (parser) => {
+  const startDate = new Date().getTime()
+  const {url, quotes} = parser
+  const result = {
+    quotes: {}
+  }
+
+  if (!url) {
+    return null
+  }
+
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  await page.emulate(iPad)
+  await page.setRequestInterception(true)
+
+  page.on('request', req => {
+    if (req.resourceType() === 'font' || req.resourceType() === 'image') {
+      req.abort()
+    } else {
+      req.continue()
+    }
+  })
+
+  try {
+    await page.goto(url, {waitUntil: 'networkidle2'})
+
+    for (let quote of quotes) {
+      const {code, xPaths} = quote
+      const listOfKeys = Object.keys(xPaths)
+      result['quotes'][code] = {}
+
+      for (let key of listOfKeys) {
+        const handle = await page.$x(xPaths[key])
+        result['quotes'][code][key] = await page.evaluate(el => el.textContent.toLowerCase(), handle[0])
+
+        // Take screenshot.
+        // const fileName = url.replace(/([-=.:/%?#])/g, '_')
+        // await handle[0].screenshot({
+        //   path: `${__dirname}/screenshots/${fileName}_${code}_${key}.jpeg`
+        // })
+      }
+    }
+  } catch (err) {
+    console.log(`An error occurred on ${url}`)
+  } finally {
+    await page.close()
+  }
+
+  await browser.close()
+
+  result.time = `${Math.round((new Date().getTime() - startDate) / 1000)} s`
+  return result
+}
+
+module.exports = runCrawler
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const express = require('express')
 const expressGraphql = require('express-graphql')
 const schema = require('./graphql')
+const runCrawler = require('./crawler')
 
 const port = parseInt(process.env.PORT, 10) || 4000
 const server = express()
@@ -23,12 +24,48 @@ mongoose.connect(
 server
   .use(
     '/graphql',
-    expressGraphql(() => ({
+    expressGraphql({
       schema,
-      context: { startTime: Date.now() },
+      context: {startTime: Date.now()},
       pretty: true,
       graphiql: true
-    }))
+    })
+  )
+
+const parser = {
+  url: 'http://bnm.md/ro/content/ratele-de-schimb',
+  quotes: [
+    {
+      code: 'usd',
+      xPaths: {
+        bid: '//*[@id="ajax-wrapper-table"]/div[1]/div[1]/table[2]/tbody/tr[12]/td[5]/div/span',
+        ask: '//*[@id="ajax-wrapper-table"]/div[1]/div[1]/table[2]/tbody/tr[12]/td[5]/div/span',
+        currency: '//*[@id="ajax-wrapper-table"]/div[1]/div[1]/table[2]/tbody/tr[12]/td[3]',
+      }
+    },
+    {
+      code: 'eur',
+      xPaths: {
+        bid: '//*[@id="ajax-wrapper-table"]/div[1]/div[1]/table[2]/tbody/tr[17]/td[5]/div/span',
+        ask: '//*[@id="ajax-wrapper-table"]/div[1]/div[1]/table[2]/tbody/tr[17]/td[5]/div/span',
+        currency: '//*[@id="ajax-wrapper-table"]/div[1]/div[1]/table[2]/tbody/tr[17]/td[3]',
+      }
+    },
+  ]
+}
+
+// Crawler server route.
+server
+  .use(
+    '/crawler',
+    async (req, res, next) => {
+      try {
+        const data = await runCrawler(parser)
+        res.json(data)
+      } catch (error) {
+        next(error)
+      }
+    }
   )
 
 server
@@ -37,7 +74,7 @@ server
       throw err
     }
 
-    console.log(`🎉  Ready on http://localhost:${port}/graphql`)
+    console.log(`🎉  Ready on http://localhost:${port}.`)
   })
 
 module.exports = server
