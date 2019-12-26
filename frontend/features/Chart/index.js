aedb00111a030fd561dcb04596685d67e9a21b42
alexei.seremet <alexei.seremet@gmail.com>
2019-12-26 20:32:40 +0200

    Fix chart scroll.

--- frontend/features/Chart/index.js
+++ frontend/features/Chart/index.js
@@ -17,14 +17,16 @@ const CustomizedAxisTick = ({ x, y, payload }) => {
 };
 
 const Chart = ({ id, data }) => (
-  <BarChart width={728} height={96} data={data} id={`chart-${id}`} margin={{ top: 14 }} barCategoryGap={'5%'}>
-    <XAxis dataKey="date" tick={<CustomizedAxisTick/>}
-           tickLine={{ size: 10, stroke: '#aaa' }} axisLine={{ stroke: '#aaa' }}
-    />
-    <YAxis type="number" domain={['dataMin', 'dataMax']} hide={true} />
-    <Bar dataKey='bid' fill="#ffdd05" fillOpacity="0.4" isAnimationActive={false} minPointSize={3}
-         label={{ position: 'top', marginTop: 20, fontSize: 10 }} />
-  </BarChart>
+  <div style={{ width: '100%', overflowX: 'auto', overflowY: 'hidden' }}>
+    <BarChart width={728} height={96} data={data} id={`chart-${id}`} margin={{ top: 14 }} barCategoryGap={'5%'}>
+      <XAxis dataKey="date" tick={<CustomizedAxisTick/>}
+             tickLine={{ size: 10, stroke: '#aaa' }} axisLine={{ stroke: '#aaa' }}
+      />
+      <YAxis type="number" domain={['dataMin', 'dataMax']} hide={true} />
+      <Bar dataKey='bid' fill="#ffdd05" fillOpacity="0.4" isAnimationActive={false} minPointSize={3}
+           label={{ position: 'top', marginTop: 20, fontSize: 10 }} />
+    </BarChart>
+  </div>
 );
 
 export default Chart;
