306c7cb233b21a4675b06955026310d5c33c30fc
alexei.seremet <alexei.seremet@gmail.com>
2020-03-14 20:16:36 +0200

    Change TLD.

--- frontend/pages/global/country.js
+++ frontend/pages/global/country.js
@@ -36,7 +36,7 @@ const CountryPageMarkup = ({
         <div className="page-lead" style={{ marginBottom: '3rem' }}>
           <BestQuotes
             centralQuote={centralQuote}
-            noLink={`${query.slug}.xezoom.com/${i18n.lng}`}
+            noLink={`${query.slug}.exchangerbee.com/${i18n.lng}`}
             {... { baseCurrenciesArr }}
           />
         </div>
