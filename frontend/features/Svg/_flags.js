9140031242c4e2580dee4bdf8214fbcb270e1b62
alexei.seremet <alexei.seremet@gmail.com>
2020-12-20 19:45:20 +0200

    Fix flags issues.

--- frontend/features/Svg/_flags.js
+++ frontend/features/Svg/_flags.js
@@ -7,3 +7,7 @@ import eur from '../../assets/images/flags/eur.svg?sprite';
 import gbp from '../../assets/images/flags/gbp.svg?sprite';
 import jpy from '../../assets/images/flags/jpy.svg?sprite';
 import mdl from '../../assets/images/flags/mdl.svg?sprite';
+import ron from '../../assets/images/flags/ron.svg?sprite';
+import rub from '../../assets/images/flags/rub.svg?sprite';
+import uah from '../../assets/images/flags/uah.svg?sprite';
+import usd from '../../assets/images/flags/usd.svg?sprite';
